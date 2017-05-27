c80a62782b22b299aa0df06101bdae1c8966f188
Brandon <idhere703@gmail.com>
2017-05-26 23:19:10 -0400

    Actually added the store for add contact.

--- src/data/AppStore.js
+++ src/data/AppStore.js
@@ -2,6 +2,14 @@ import Immutable from 'immutable';
 import { ReduceStore } from 'flux/utils';
 import AppActionTypes from './AppActionTypes';
 import AppDispatcher from './AppDispatcher';
+import Contact from './Contact';
+
+let _counter = 1;
+const Counter = {
+    increment() {
+        return 'id-' + String(_counter++);
+    },
+};
 
 class AppStore extends ReduceStore {
     constructor() {
@@ -15,8 +23,18 @@ class AppStore extends ReduceStore {
     reduce(state, action) {
         switch (action.type) {
             case AppActionTypes.ADD_CONTACT:
-                // Do nothing for now.
-                return state;
+                // Don't add contacts with missing data with no text.
+                if (!action.email || !action.name || action.phone) {
+                    return state;
+                }
+
+                const id = Counter.increment();
+                return state.set(id, new Contact({
+                    id,
+                    email: action.email,
+                    name: action.name,
+                    phone: action.phone,
+                }));
 
             default:
                 return state;
